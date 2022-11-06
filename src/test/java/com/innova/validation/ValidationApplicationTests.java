5ce77f09917696333eed1ee3abf3d773d66841b4
Boying <shallmay14@gmail.com>
2022-11-06 23:02:16 +0800

    [SD] init ValidationApplication

---
+++ src/test/java/com/innova/validation/ValidationApplicationTests.java
@@ -0,0 +1,11 @@
+package com.innova.validation;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+public class ValidationApplicationTests {
+    @Test
+    void contextLoads() {
+    }
+}
